a6f4e6accde2a2224931a699acbe4f3789f3600b
Ash <sgt16@yandex.ru>
2021-07-24 20:34:42 +0300

    2. Тестовое задание [#504790]

--- src/main/java/ru/job4j/articles/service/SimpleArticleService.java
+++ src/main/java/ru/job4j/articles/service/SimpleArticleService.java
@@ -28,6 +28,8 @@ public class SimpleArticleService implements ArticleService {
                 .peek(i -> LOGGER.info("Сгенерирована статья № {}", i))
                 .mapToObj((x) -> articleGenerator.generate(words))
                 .collect(Collectors.toList());
-        articles.forEach(articleStore::save);
+        for (var i : articles) {
+            articleStore.save((Article) i.get());
+        }
     }
 }
--- src/main/java/ru/job4j/articles/service/generator/ArticleGenerator.java
+++ src/main/java/ru/job4j/articles/service/generator/ArticleGenerator.java
@@ -1,10 +1,10 @@
 package ru.job4j.articles.service.generator;
 
-import ru.job4j.articles.model.Article;
 import ru.job4j.articles.model.Word;
 
+import java.lang.ref.SoftReference;
 import java.util.List;
 
 public interface ArticleGenerator {
-    Article generate(List<Word> words);
+    SoftReference generate(List<Word> words);
 }
