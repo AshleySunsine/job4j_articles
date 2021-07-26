da8d0d1e2bfa8858e5d2650c37f808cd762ed710
Ash <sgt16@yandex.ru>
2021-07-27 01:37:42 +0300

    2. Тестовое задание [#504790 #181365]

--- src/main/java/ru/job4j/articles/service/generator/ArticleGenerator.java
+++ src/main/java/ru/job4j/articles/service/generator/ArticleGenerator.java
@@ -1,10 +1,11 @@
 package ru.job4j.articles.service.generator;
 
+import ru.job4j.articles.model.Article;
 import ru.job4j.articles.model.Word;
 
 import java.lang.ref.SoftReference;
 import java.util.List;
 
 public interface ArticleGenerator {
-    SoftReference generate(List<Word> words);
+    Article generate(List<Word> words);
 }
